1a53fe5aad522af6e674b018f92e4aacea45eb5d
richard <dev.projects101@gmail.com>
2023-05-05 12:28:16 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/backend/model/ToDo.java
@@ -0,0 +1,16 @@
+package de.neuefische.backend.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ToDos {
+
+    private String toDo;
+
+}
