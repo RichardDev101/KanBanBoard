5d1f06bb0608c02213e04994984b9b342c99a9ea
richard <dev.projects101@gmail.com>
2023-05-05 16:35:52 +0200

    Second integration test

--- src/test/java/de/neuefische/backend/controller/KanbanControllerTest.java
+++ src/test/java/de/neuefische/backend/controller/KanbanControllerTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,22 +19,31 @@ class KanbanControllerTest {
     MockMvc mockMvc;
 
     @Test
-    void listToDos() {
+    void whenGetList_returnToDosList_and_200() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/todo"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
     }
 
     @Test
-    void addToDos() {
-    }
+        void whenAddOrder_returnOrderWithId_andStatusCode200_version() throws Exception {
+            mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
+                            .contentType("application/json")
+                            .content("""
+                                        {
+                                        "description": "1",
+                                        "status": "Apfel"
+                                        }
+                                    """))
+                    .andExpect(status().isOk());
 
-    @Test
-    void getToDo() {
-    }
-
-    @Test
-    void editToDos() {
-    }
-
-    @Test
-    void deleteToDos() {
-    }
+            mockMvc.perform(MockMvcRequestBuilders.get("/api/todo"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().json("""
+                                        [{
+                                        "description": "1",
+                                        "status": "Apfel"
+                                        }]
+                                    """)).andExpect(jsonPath("$[0].id").isNotEmpty());
+        }
 }
