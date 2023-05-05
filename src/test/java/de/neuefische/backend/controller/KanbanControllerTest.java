83eb00845f83595f32969966f72524b146c9b177
richard <dev.projects101@gmail.com>
2023-05-05 19:05:09 +0200

    all integration tests finished

--- src/test/java/de/neuefische/backend/controller/KanbanControllerTest.java
+++ src/test/java/de/neuefische/backend/controller/KanbanControllerTest.java
@@ -36,26 +36,21 @@ class KanbanControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
                         .contentType("application/json")
                         .content("""
-                                    {
-                                    "description": "Datenbanken einrichten",
-                                    "status": "Open"
-                                    }
+                            {"description": "Hello","status": "Open"}
                                 """))
                 .andExpect(status().isOk());
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/todo"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
-                            [{
-                            "description": "1",
-                            "status": "Apfel"
-                            }]
-                        """)).andExpect(jsonPath("$[0].id").isNotEmpty());
+                            [{"description": "Hello", "status": "Open"}]
+                        """))
+                .andExpect(jsonPath("$[0].id").isNotEmpty());
     }
 
     @Test
     @DirtiesContext
-    void whenDetails_returnToDoById_and_200ok() throws Exception {
+    void whenGetToDo_returnToDoById_and_200ok() throws Exception {
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
@@ -105,6 +100,46 @@ class KanbanControllerTest {
                 .andExpect(content().json("""
                                     {"description":"Aenderung","status":"OPEN"}
                                 """));
+    }
+
+    @Test
+    void whenDeleteToDos_returnToDoWithoutItemById_and_200ok() throws Exception {
+        MvcResult result1 = mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                   {"description":"12", "status":"OPEN"}
+                                """))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content1 = result1.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper1 = new ObjectMapper();
+        ToDo toDo1 = objectMapper1.readValue(content1, ToDo.class);
+
+        MvcResult result2 = mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                   {"description":"34", "status":"OPEN"}
+                                """))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content2 = result2.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper2 = new ObjectMapper();
+        ToDo toDo2 = objectMapper2.readValue(content2, ToDo.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/todo/"+ toDo1.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                   {"description":"12", "status":"OPEN"}
+                                """))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                                    [{"description":"34", "status":"OPEN"}]
+                                """))
+                .andExpect(jsonPath("$[0].id").isNotEmpty());
     }
 
 }
